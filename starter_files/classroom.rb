8d9eb4ba6283a63d1486f0a4221f84365f0ee588
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-13 19:58:30 -0400

    Added instructor solutions for review/practice

--- starter_files/classroom.rb
+++ starter_files/classroom.rb
@@ -2,21 +2,47 @@
 # for that student and assignment. Note that Ruby counts arrays from 0, but
 # we are referring to them as 1-10.
 
+def assignment_score(data, name, assignment)
+    data[:name][assignment -1]
+end
 
 # Given a grade_hash and assignment number, return all scores for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 
+def test_assignment_scores(grade_hash, assignment_num)
+    grade.hash.values.map { |scores| scores[assignment - 1] }
+
+    grade.hash.values.map do |scores|
+        scores[assignment_num -1]
+    end
+end
 
 # Given a grade_hash and assignment number, return the average score for that
 # assignment. Note that Ruby counts arrays from 0, but we are referring to
 # them as 1-10.
 
+def test_assignment_average_score(grade_hash, assignment_avg)
+    grade.hash.values.map do |scores| + |scores|
+        average[|scores|.sum/sum]
+end
+
+# Amy's Solution:
+# def assignment_average_score(grade_hash, assignment_num)
+#     scores = assignment_scores(grade_hash, assignment_num)
+#     scores.sum / scores.length
+# end
 
 # Return a hash of students and their average score.
 # TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
 # use .to_h. Also look at Hash#transform_values.
 
+# Amy's Solution
+# def averages(grade_hash)
+#     grade_hash.transform_values do |grades|
+#       grades.sum / grades.length
+#     end
+# end
 
 # Return a letter grade for a numerical score.
 # 90+ => A
@@ -25,12 +51,44 @@
 # 60-69 => D
 # < 60 => F
 
+# Amy's Solution
+# def letter_grade(score)
+#     if score >= 90
+#       "A"
+#     elsif score >= 80
+#       "B"
+#     elsif score >= 70
+#       "C"
+#     elsif score >= 60
+#       "D"
+#     else
+#       "F"
+#     end
+# end
+
 # Return a hash of students and their final letter grade, as determined
 # by their average.
 
+# Amy's Solution
+# def final_letter_grades(grade_hash)
+#     averages(grade_hash).transform_values { |score| letter_grade(score) }
+# end
 
 # Return the average for the entire class.
 
+# Amy's Solution
+# def class_average(grade_hash)
+#     scores = averages(grade_hash).values
+#     scores.sum / scores.length
+# end
 
 # Return an array of the top `number_of_students` students.
 
+# Amy's Solutiion
+# def top_students(grade_hash, number_of_students)
+#     averages(grade_hash)
+#       .to_a
+#       .sort_by { |student| -student[1] }
+#       .map { |student| student[0] }
+#       .take(number_of_students)
+# end
